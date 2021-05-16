3c09ca3ca79d4632905e6ffe55a15d95f36b7d59
LashaunnaS <shaunna191@icloud.com>
2021-05-17 01:38:12 +0200

    Update styles.

--- src/shared/styledComponents/GridLayout.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-const GridLayout = styled.main`
-  width: 60vw;
-  height: 100vh;
-  background-color: #ffffff;
-`;
-
-export default GridLayout;
--- src/shared/styledComponents/ListLayout.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-const ListLayout = styled.main`
-  width: 40vw;
-  height: 100vh;
-  background-color: #37374a;
-`;
-
-export default ListLayout;
