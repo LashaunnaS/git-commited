2f01003042bfa7755063fb1a0b50f1b8af71a39b
LashaunnaS <shaunna191@icloud.com>
2021-05-19 20:25:39 +0200

    Update padding to match design spacing.

--- src/App/components/ListLayout/ListLayoutStyles.js
+++ src/App/components/ListLayout/ListLayoutStyles.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 const ListLayoutStyles = styled.article`
-  padding: 50px 30px;
+  padding: 70px 30px 0;
   width: 40vw;
   height: 100vh;
   background-color: #37374a;
