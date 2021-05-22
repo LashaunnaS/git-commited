7e5737aba38787d280b979dd36e3c4196bc3aaa6
LashaunnaS <shaunna191@icloud.com>
2021-05-22 18:16:41 +0200

    Moved from shared to components/

--- src/App/components/ListLayout/ListLayoutStyles.js
+++ src/App/components/ListLayout/ListLayoutStyles.js
@@ -3,8 +3,9 @@ import styled from 'styled-components';
 const ListLayoutStyles = styled.article`
   padding: 70px 30px 0;
   width: 40vw;
-  height: 100vh;
   background-color: #37374a;
+  height: max-content;
+  min-height: 100vh;
 `;
 
 export default ListLayoutStyles;
