625b03f6a80a9cc810d8feab695d1d4fc00ead12
LashaunnaS <shaunna191@icloud.com>
2021-05-25 19:34:24 +0200

    format last updated notice. update colors to matcch brand

--- src/App/components/ListLayout/ListItem/index.tsx
+++ src/App/components/ListLayout/ListItem/index.tsx
@@ -1,6 +1,8 @@
 import { useQuery } from '@apollo/client';
 import React from 'react';
 import { Star, Trash2 } from 'react-feather';
+import formatDistance from 'date-fns/formatDistance';
+import format from 'date-fns/format/index';
 import QUERY_REPOSITORY_STATS from '../../../../shared/queries/repositoryStats';
 import RepositoryNameStyles from '../../../../shared/styledComponents/RepositoryNameStyles';
 import ListItemColorStripStyles from './ListItemColorStripStyles/ListItemColorStripStyles';
@@ -8,15 +10,17 @@ import ListItemStyles from './ListItemStyles';
 import ListItemNameStyles from './ListItemNameStyles/ListItemNameStyles';
 import ListItemStarsStyles from './ListItemStarsStyles/ListItemStarsStyles';
 import ListItemStatsStyles from './ListItemStatsStyles/ListItemStatsStyles';
+import ListItemUpdateTextStyles from './ListItemUpdateTextStyles/ListItemUpdateTextStyles';
 import ListItemDeleteButtonStyles from './ListItemDeleteButtonStyles/ListItemDeleteButtonStyles';
+import { Selection } from '../../..';
 
 interface ListItemProps {
-  repository: string;
-  removeRepository: (repo: string) => void;
+  repository: Selection;
+  removeRepository: (repo: Selection) => void;
 }
 
 const ListItem = ({ repository, removeRepository }: ListItemProps): JSX.Element => {
-  const [repoOwner, repoName] = repository.split('/');
+  const [repoOwner, repoName] = repository.nameWithOwner.split('/');
 
   const { data } = useQuery(QUERY_REPOSITORY_STATS, {
     variables: { repoOwner, repoName },
@@ -32,8 +36,21 @@ const ListItem = ({ repository, removeRepository }: ListItemProps): JSX.Element 
     return num;
   };
 
+  const lastUpdated = () => {
+    const currDate = new Date();
+    const lastUpdatedDate = data?.repository.updatedAt;
+    const lastUpdatedInYear = format(new Date(lastUpdatedDate), 'YYY');
+
+    if (currDate.getFullYear() - Number(lastUpdatedInYear) < 1) {
+      return `Updated ${formatDistance(new Date(), new Date(data?.repository.updatedAt), {
+        includeSeconds: true,
+      })} ago`;
+    }
+    return `Updated on ${format(new Date(lastUpdatedDate), 'MMM dd, YYY')}`;
+  };
+
   const formatRepositoryName = () => {
-    const repo = repository.split('/');
+    const repo = repository.nameWithOwner.split('/');
 
     return (
       <ListItemNameStyles>
@@ -45,22 +62,20 @@ const ListItem = ({ repository, removeRepository }: ListItemProps): JSX.Element 
 
   return (
     <ListItemStyles>
-      <ListItemColorStripStyles />
+      <ListItemColorStripStyles color={repository.color} />
       <ListItemStatsStyles>
         {formatRepositoryName()}
         <div>
-          <Star color="#fff" size="13" />
+          <Star color="var(--color-white-regular)" size="13" />
           <ListItemStarsStyles>{stars()}</ListItemStarsStyles>
-          <span style={{ color: '#bfbdd9', fontSize: '1.3rem' }}>
-            {data?.repository.updatedAt}
-          </span>
+          <ListItemUpdateTextStyles>{data && lastUpdated()}</ListItemUpdateTextStyles>
         </div>
       </ListItemStatsStyles>
       <ListItemDeleteButtonStyles
         type="button"
         onClick={() => removeRepository(repository)}
       >
-        <Trash2 color="#fff" />
+        <Trash2 color="var(--color-white-regular)" />
       </ListItemDeleteButtonStyles>
     </ListItemStyles>
   );
