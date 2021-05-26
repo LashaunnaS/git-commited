2b311dd641016548209084269723dd22c31d2b02
LashaunnaS <shaunna191@icloud.com>
2021-05-26 19:35:22 +0200

    update types and check data for duplicates.

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
+import axios, { AxiosResponse } from 'axios';
 import { Repository } from '../generated/graphql';
 import GlobalStyle from '../shared/styles/GlobalStyles';
 import AppLayout from './AppLayoutStyles';
@@ -16,16 +17,46 @@ const client = new ApolloClient({
   cache: new InMemoryCache(),
 });
 
+interface Commits {
+  days: Array<number>;
+  week: number;
+  total: number;
+}
+
 export interface Selection extends Pick<Repository, 'nameWithOwner'> {
   id: number;
   color: string;
+  commits?: Array<Commits>;
 }
 
 const App = (): JSX.Element => {
   const [selection, setSelection] = useState<Array<Selection>>([]);
 
-  const addRepository = (newRepo: Selection): void =>
-    setSelection((oldRepoList) => [...oldRepoList, newRepo]);
+  // : Promise<AxiosResponse<Array<Commits>>>
+  const fetchRepoCommits = async (repositoryName: string) => {
+    const [owner, name] = repositoryName.split('/');
+    // eslint-disable-next-line max-len
+    const gitHubUrl = `https://api.github.com/repos/${owner}/${name}/stats/commit_activity`;
+    const response = await axios.get(gitHubUrl);
+
+    return response.data;
+  };
+
+  const repoExists = (newRepo: Selection) => {
+    return selection.some((el) => {
+      return el.nameWithOwner === newRepo.nameWithOwner;
+    });
+  };
+
+  const addRepository = async (newRepo: Selection): Promise<void> => {
+    if (!repoExists(newRepo)) {
+      const commitData = await fetchRepoCommits(newRepo.nameWithOwner);
+      setSelection((oldRepoList) => [
+        ...oldRepoList,
+        { ...newRepo, commits: commitData },
+      ]);
+    }
+  };
 
   const removeRepository = (repo: Selection): void => {
     const newSelection = selection.filter((item) => item !== repo);
@@ -35,7 +66,7 @@ const App = (): JSX.Element => {
   return (
     <ApolloProvider client={client}>
       <AppLayout>
-        <GridLayout />
+        <GridLayout selection={selection} />
         <ListLayout
           addRepository={addRepository}
           selection={selection}
