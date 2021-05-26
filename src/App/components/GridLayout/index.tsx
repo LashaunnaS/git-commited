43624f9dd11c7c89c8ecac4b994e7d5b88082f30
LashaunnaS <shaunna191@icloud.com>
2021-05-26 17:22:23 +0200

    Add Grid and format data to provide grid.

--- src/App/components/GridLayout/index.tsx
+++ src/App/components/GridLayout/index.tsx
@@ -1,10 +1,49 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { format, fromUnixTime } from 'date-fns';
+import { Selection } from '../..';
+import defaultData from './data';
+import MyResponsiveLine from './MyResponsiveLine';
 import GridLayoutStyles from './GridLayoutStyles';
 
-const GridLayout = (): JSX.Element => {
+interface GridDataProps {
+  id: string;
+  color: string;
+  data: Array<{ x: string; y: number }> | undefined;
+}
+
+interface GridLayoutProps {
+  selection: Array<Selection>;
+}
+
+const GridLayout = ({ selection }: GridLayoutProps): JSX.Element => {
+  const [repoStats, setRepoStats] = useState<Array<GridDataProps>>([]);
+
+  const graphDataFormat = (): void => {
+    const updatedDataKeys: Array<GridDataProps> = selection.map((item) => {
+      return {
+        id: item.nameWithOwner,
+        color: item.color,
+        data: item?.commits?.map((value) => {
+          const theDate = fromUnixTime(value.week);
+
+          return {
+            x: format(theDate, 'yyyy-MM-dd'),
+            y: value.total,
+          };
+        }),
+      };
+    });
+
+    setRepoStats(updatedDataKeys);
+  };
+
+  useEffect(() => {
+    graphDataFormat();
+  }, [selection]);
+
   return (
     <GridLayoutStyles>
-      <p>Grid Area</p>
+      <MyResponsiveLine data={repoStats.length === 0 ? defaultData : repoStats} />
     </GridLayoutStyles>
   );
 };
