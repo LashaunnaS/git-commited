d77fd25f0a93af965645408223ea5d2c2064d7e2
LashaunnaS <shaunna191@icloud.com>
2021-05-26 18:06:27 +0200

    Update styles.

--- src/App/components/ListLayout/ListLayoutStyles.js
+++ src/App/components/ListLayout/ListLayoutStyles.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 const ListLayoutStyles = styled.article`
-  background-color: #37374a;
+  background-color: var(--color-purple-dark);
   height: 100vh;
   overflow-y: auto;
   padding: 7rem 3rem 3rem;
