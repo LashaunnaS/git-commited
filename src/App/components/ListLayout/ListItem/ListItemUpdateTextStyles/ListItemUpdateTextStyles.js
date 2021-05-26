89b24e6fd0980f05707d1e8c74147ed78abb337e
LashaunnaS <shaunna191@icloud.com>
2021-05-26 19:33:26 +0200

    Addes styles for list item content cards.

---
+++ src/App/components/ListLayout/ListItem/ListItemUpdateTextStyles/ListItemUpdateTextStyles.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+const ListItemUpdateTextStyles = styled.span`
+  color: var(--color-purple-lighter);
+  font-size: var(--font-size-smaller);
+`;
+
+export default ListItemUpdateTextStyles;
