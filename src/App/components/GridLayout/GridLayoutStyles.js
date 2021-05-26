1d8dc83707b32cd6191764d7ee21e7d046bed5a0
LashaunnaS <shaunna191@icloud.com>
2021-05-26 17:21:49 +0200

    Update styles.

--- src/App/components/GridLayout/GridLayoutStyles.js
+++ src/App/components/GridLayout/GridLayoutStyles.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 const GridLayoutStyles = styled.article`
   width: 60vw;
   height: 100vh;
-  background-color: #ffffff;
+  background-color: var(--color-white-regular);
 `;
 
 export default GridLayoutStyles;
